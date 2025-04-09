4db0896461b8a78cb5d8ecfb15f177c59af2d6e0
beninburley <130599399+beninburley@users.noreply.github.com>
2025-04-09 10:48:25 -0600

    Add hsts requirment while in production

--- backend/CineNiche.API/Program.cs
+++ backend/CineNiche.API/Program.cs
@@ -105,6 +105,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseHsts();
 }
 
 app.UseCors("AllowFrontend"); // ? now it's in the right spot
