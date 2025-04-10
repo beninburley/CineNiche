fe063a61111bf98d067033f1c911a68286c0b990
Jo_Jo_from_Mars <joshuayang99@gmail.com>
2025-04-10 17:24:55 -0600

    the link and the scrolling bar

--- backend/CineNiche.API/obj/Debug/net8.0/CineNiche.API.AssemblyInfo.cs
+++ backend/CineNiche.API/obj/Debug/net8.0/CineNiche.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineNiche.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88b309f6e08812a4b3f98f5c85e30392b4d9e79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ee5efef98ae360d617bdfd575d7a6fb9902a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineNiche.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineNiche.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
